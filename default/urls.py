2d6615e86c401429bf315e66132bdc6e31207ef5
Yamo <hsiouming911019@gmail.com>
2019-04-17 15:41:42 +0800

    投票完成

--- default/urls.py
+++ default/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     #path('poll/', poll_list),
     path('poll/',PollList.as_view()),
-    path('poll/<int:pk>/', PollDetail.as_view())
+    path('poll/<int:pk>/', PollDetail.as_view()),
+    path('vote/<int:oid>/',PollVote.as_view()),
 ]
